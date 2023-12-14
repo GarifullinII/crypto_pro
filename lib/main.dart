f6c932546b74caf9e951f5a9c2e915e5c4a31fc6
GarifullinII <ildaridrisovich@gmail.com>
2023-12-14 22:58:10 +0400

    refactor, add crypto_pro file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:crypto_pro/router/router.dart';
-import 'package:crypto_pro/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'crypto_pro.dart';
 
 void main() {
   runApp(const CryptoPro());
-}
-
-class CryptoPro extends StatelessWidget {
-  const CryptoPro({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Crypto PRO',
-      theme: darkTheme,
-      routes: routes,
-    );
-  }
 }
