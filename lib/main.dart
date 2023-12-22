1e4b491dc52b0207cba589ff6411282dc2e608a6
GarifullinII <ildaridrisovich@gmail.com>
2023-12-22 23:34:26 +0400

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:crypto_pro/repositories/crypto_coins/abstract_coins_repository.d
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 import 'crypto_pro.dart';
 import 'repositories/crypto_coins/crypto_coins_repository.dart';
 
 void main() {
+  final talker = TalkerFlutter.init();
+  GetIt.I.registerSingleton(talker);
+  GetIt.I<Talker>().debug('Talker started...');
   GetIt.I.registerSingleton<AbstractCoinsRepository>(CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoPro());
 }
